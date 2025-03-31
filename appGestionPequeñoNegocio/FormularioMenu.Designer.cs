f61bf3f7f1b7128c8291204c4383fd2493f35fac
sebaacc <sebaacc11@gmail.com>
2025-03-31 12:31:22 -0300

    Agregar archivos de proyecto.

---
+++ appGestionPequeñoNegocio/FormularioMenu.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace appGestionPequeñoNegocio
+{
+    partial class FormularioMenu
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnCargar = new System.Windows.Forms.Button();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.lblDescripcion = new System.Windows.Forms.Label();
+            this.txtDescripcion = new System.Windows.Forms.TextBox();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.lblEliminar = new System.Windows.Forms.Label();
+            this.txtCodigoEliminar = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(35, 9);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(206, 24);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "ABM para tu negocio";
+            // 
+            // btnCargar
+            // 
+            this.btnCargar.Location = new System.Drawing.Point(38, 369);
+            this.btnCargar.Name = "btnCargar";
+            this.btnCargar.Size = new System.Drawing.Size(100, 23);
+            this.btnCargar.TabIndex = 1;
+            this.btnCargar.Text = "Cargar";
+            this.btnCargar.UseVisualStyleBackColor = true;
+            this.btnCargar.Click += new System.EventHandler(this.btnCargar_Click);
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(38, 66);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(252, 50);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.Size = new System.Drawing.Size(507, 292);
+            this.dgvDatos.TabIndex = 3;
+            this.dgvDatos.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvDatos_CellContentClick);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(684, 369);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 4;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(35, 50);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(44, 13);
+            this.lblNombre.TabIndex = 5;
+            this.lblNombre.Text = "Nombre";
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(35, 101);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(40, 13);
+            this.lblCodigo.TabIndex = 7;
+            this.lblCodigo.Text = "Codigo";
+            this.lblCodigo.Click += new System.EventHandler(this.lblApellido_Click);
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(38, 117);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 6;
+            // 
+            // lblDescripcion
+            // 
+            this.lblDescripcion.AutoSize = true;
+            this.lblDescripcion.Location = new System.Drawing.Point(35, 153);
+            this.lblDescripcion.Name = "lblDescripcion";
+            this.lblDescripcion.Size = new System.Drawing.Size(63, 13);
+            this.lblDescripcion.TabIndex = 9;
+            this.lblDescripcion.Text = "Descripción";
+            // 
+            // txtDescripcion
+            // 
+            this.txtDescripcion.Location = new System.Drawing.Point(38, 169);
+            this.txtDescripcion.Name = "txtDescripcion";
+            this.txtDescripcion.Size = new System.Drawing.Size(100, 20);
+            this.txtDescripcion.TabIndex = 8;
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(35, 204);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(37, 13);
+            this.lblPrecio.TabIndex = 11;
+            this.lblPrecio.Text = "Precio";
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(38, 220);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 10;
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(35, 257);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(54, 13);
+            this.lblCategoria.TabIndex = 13;
+            this.lblCategoria.Text = "Categoría";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.Location = new System.Drawing.Point(38, 273);
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.Size = new System.Drawing.Size(100, 20);
+            this.txtCategoria.TabIndex = 12;
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(35, 306);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(35, 13);
+            this.lblStock.TabIndex = 15;
+            this.lblStock.Text = "Stock";
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(38, 322);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 14;
+            // 
+            // lblEliminar
+            // 
+            this.lblEliminar.AutoSize = true;
+            this.lblEliminar.Location = new System.Drawing.Point(517, 356);
+            this.lblEliminar.Name = "lblEliminar";
+            this.lblEliminar.Size = new System.Drawing.Size(147, 13);
+            this.lblEliminar.TabIndex = 17;
+            this.lblEliminar.Text = "Codigo de producto a eliminar";
+            // 
+            // txtCodigoEliminar
+            // 
+            this.txtCodigoEliminar.Location = new System.Drawing.Point(520, 372);
+            this.txtCodigoEliminar.Name = "txtCodigoEliminar";
+            this.txtCodigoEliminar.Size = new System.Drawing.Size(144, 20);
+            this.txtCodigoEliminar.TabIndex = 16;
+            // 
+            // FormularioMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblEliminar);
+            this.Controls.Add(this.txtCodigoEliminar);
+            this.Controls.Add(this.lblStock);
+            this.Controls.Add(this.txtStock);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.lblPrecio);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.lblDescripcion);
+            this.Controls.Add(this.txtDescripcion);
+            this.Controls.Add(this.lblCodigo);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btnCargar);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "FormularioMenu";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormularioMenu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnCargar;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.Label lblDescripcion;
+        private System.Windows.Forms.TextBox txtDescripcion;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.Label lblEliminar;
+        private System.Windows.Forms.TextBox txtCodigoEliminar;
+    }
+}
+
---
+++ appGestionPequeñoNegocio/FormularioMenu.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace appGestionPequeñoNegocio
+{
+    public partial class FormularioMenu : Form
+    {
+        public FormularioMenu()
+        {
+            InitializeComponent();
+        }
+
+        string codigo;
+        string nombre;
+        string descripcion;
+        decimal precio;
+        int stock;
+
+        private void dgvDatos_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void FormularioMenu_Load(object sender, EventArgs e)
+        {
+            dgvDatos.Columns.Add("Codigo", "Código");
+            dgvDatos.Columns.Add("Nombre", "Nombre");
+            dgvDatos.Columns.Add("Descripcion", "Descripción");
+            dgvDatos.Columns.Add("Precio", "Precio");
+            dgvDatos.Columns.Add("Stock", "Stock");
+        }
+
+        private void btnCargar_Click(object sender, EventArgs e)
+        {
+            cargar();
+        }
+        private void cargar()
+        {
+            try
+            {
+                // Captura los datos de los TextBox y los guarda en las variables
+                codigo = txtNombre.Text;
+                nombre = txtNombre.Text;
+                descripcion = txtDescripcion.Text;
+                precio = decimal.Parse(txtPrecio.Text); // Convierte el texto a decimal
+                stock = int.Parse(txtStock.Text); // Convierte el texto a entero
+
+                // Opcional: Puedes agregar aquí validaciones adicionales
+                // Por ejemplo, verificar que los TextBox no estén vacíos, etc.
+
+                // Agrega una fila al DataGridView con los datos capturados
+                dgvDatos.Rows.Add(codigo, nombre, descripcion, precio, stock);
+
+                // Opcional: Limpia los TextBox después de agregar la fila
+                txtNombre.Text = "";
+                txtNombre.Text = "";
+                txtDescripcion.Text = "";
+                txtPrecio.Text = "";
+                txtStock.Text = "";
+            }
+            catch (FormatException ex)
+            {
+                // Maneja el error si el usuario ingresa datos no válidos (por ejemplo, texto en un campo numérico)
+                MessageBox.Show("Error: Ingresa datos válidos en los campos numéricos.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Opcional: Puedes registrar el error en un archivo de registro, etc.
+            }
+            catch (Exception ex)
+            {
+                // Maneja cualquier otro error inesperado
+                MessageBox.Show("Ocurrió un error inesperado: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                // Opcional: Puedes registrar el error en un archivo de registro, etc.
+            }
+        }
+
+        private void eliminar()
+        {
+            try
+            {
+                // Obtiene el código a eliminar del TextBox
+                string codigoEliminar = txtCodigoEliminar.Text;
+
+                // Itera a través de las filas del DataGridView
+                foreach (DataGridViewRow row in dgvDatos.Rows)
+                {
+                    // Verifica si el código de la fila coincide con el código a eliminar
+                    if (row.Cells["Codigo"].Value != null && row.Cells["Codigo"].Value.ToString() == codigoEliminar)
+                    {
+                        // Elimina la fila
+                        dgvDatos.Rows.Remove(row);
+
+                        // Opcional: Muestra un mensaje de confirmación
+                        MessageBox.Show("Producto con código " + codigoEliminar + " eliminado.", "Información", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Limpia el TextBox de código a eliminar.
+                        txtCodigoEliminar.Text = "";
+
+                        // Sale del bucle, ya que se encontró y eliminó la fila
+                        return;
+                    }
+                }
+
+                // Si no se encontró el código, muestra un mensaje de error
+                MessageBox.Show("No se encontró ningún producto con el código " + codigoEliminar + ".", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                // Maneja cualquier error inesperado
+                MessageBox.Show("Ocurrió un error al eliminar el producto: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void lblApellido_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            eliminar();
+        }
+    }
+}
